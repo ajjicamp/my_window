ef813b3c09029d474771496bd88f236b80fba35b
ajjicamp <cwhur@naver.com>
2021-08-02 14:43:07 +0900

    initial

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import sys
+import sys
+from PyQt5 import QtWidgets, QAxContainer, uic
+form_class = uic.loadUiType('mywindow.ui')[0]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class MyWindow(QtWidgets.QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QtWidgets.QApplication(sys.argv)
+    mywindow = MyWindow()
+    mywindow.show()
+    app.exec_()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
