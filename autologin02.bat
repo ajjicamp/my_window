66d2f0b6c9987a1db08cf579fef66e34106dadba
ajjicamp <cwhur0616@gmail.com>
2021-09-27 09:48:38 +0900

    9/22 commit

---
+++ autologin02.bat
@@ -0,0 +1,6 @@
+@echo off
+python C:\Users\USER\PycharmProjects\my_window\login\autologin.py 4
+python C:\Users\USER\PycharmProjects\my_window\get_data\chart_data.py 2
+@echo "number2 process start"
+pause
+
